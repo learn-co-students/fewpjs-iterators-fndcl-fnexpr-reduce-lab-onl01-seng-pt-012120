9fb0e916636d578c15c56e9ff4907bfbf60001b9
hornadam8 <hornadam8@gmail.com>
2020-08-10 03:14:15 -0600

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+function aggregate(sum, ele)
+{
+  return sum += ele
+}
+
+const totalBatteries = batteryBatches.reduce(aggregate,0)
